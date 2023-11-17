6ec7ad21f4ecb97a0ecf70ad1461314d8a5f8117
Sergio de la Barrera García <sergiodlbg4esoa@gmail.com>
2023-11-17 17:30:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,21 @@ try:
     streamlit.dataframe(my_data_rows)
 except URLError as e:
   streamlit.error()
+  
+try:
+  def insert_row(new_fruit):
+    with my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+      return "Thanks for adding " + new_fruit
+
+  add_my_fruit = streamlit.text_input('What fruit would you like to add')
+  if streamlit.button('Add a fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_fuction = insert_row(add_my_fruit)
+    streamli.text(back_from_fuction)
+except URLError as e:
+  streamlit.error()
+  
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
 #my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
